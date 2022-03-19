c5cc65b50befcd2a87e27810373a62c7a585ef66
swng <stevenw1@andrew.cmu.edu>
2022-03-18 20:10:32 -0500

    replace shuffle alg with Fisher-Yates shuffle

--- zztetris.js
+++ zztetris.js
@@ -17,7 +17,15 @@ Array.prototype.getRand = function () {
 	return this[Math.floor(Math.random() * this.length)];
 };
 Array.prototype.shuffle = function () {
-	return this.sort(() => Math.random() - 0.5);
+    var i = this.length, j, temp;
+    if (i == 0) return this;
+    while ( --i ) {
+        j = Math.floor( Math.random() * ( i + 1 ) );
+        temp = this[i];
+        this[i] = this[j];
+        this[j] = temp;
+     }
+     return this;
 };
 var ctrl = {
 	ArrowLeft: 'L',
